565d3d848e2669bf3005beb3fdfd0ef5ec1d3f19
akhil <akhil2@illinois.edu>
2022-02-04 02:51:23 +0000

    Add the input form + encoding

--- src/App.js
+++ src/App.js
@@ -1,27 +1,73 @@
 import './App.css';
+import React from 'react';
 import RewordleGame from './RewordleGame';
 import RewordleContainer from './components/RewordleContainer';
+import WordleInput from './components/WordleInput';
 
 
-function App() {
-  let dummy_guesses = ["ROUSE", "STAIR", "SCARY", "SHARK", "SHARP", "SHARD"];
-  // let dummy_guesses = ["NASAL", "BANAL", "ALTER"];
-  let game = new RewordleGame(dummy_guesses);
-  // console.log(game.states);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          RE-WORDLE
-        </p>
-      </header>
-      <body className="App-body">
-        <div id='main-container'>
-          <RewordleContainer game={ game }/>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {mode: 'choose'};
+  }
+
+  render() {
+    if (this.state.mode === 'play') {
+      let dummy_guesses = ["ROUSE", "STAIR", "SCARY", "SHARK", "SHARP", "SHARD"];
+      let game = new RewordleGame(dummy_guesses);
+      return (
+        <div className="App">
+          <header className="App-header">
+            <p>
+              RE-WORDLE
+            </p>
+          </header>
+          <body className="App-body">
+    
+            <div id='main-container'>
+              <RewordleContainer game={ game }/>
+            </div>
+          </body>
         </div>
-      </body>
-    </div>
-  );
+      );
+    } else if (this.state.mode === 'input') {
+        return (
+          <div className="App">
+            <header className="App-header">
+              <p>
+                RE-WORDLE
+              </p>
+            </header>
+            <body className="App-body">
+      
+              <div id='main-container'>
+                <WordleInput />
+              </div>
+            </body>
+          </div>
+        );
+    } else {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <p>
+              RE-WORDLE
+            </p>
+          </header>
+          <body className="App-body">
+            <div id='main-container'>
+              <button id='InputChoiceButton' className='ChooseButton' onClick={() => this.setState({mode: 'input'})}>
+                Click here to make your Rewordle
+              </button>
+              <button id='PlayChoiceButton' className='ChooseButton' onClick={() => this.setState({mode: 'play'})}>
+                Click here to play a fixed Rewordle
+              </button>
+            </div>
+          </body>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
---
+++ src/components/WordleInput.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import { Buffer } from 'buffer';
+
+class WordleInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {guesses: [], textboxEntry: '', submitted: false, link: ''};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleSubmitAll = this.handleSubmitAll.bind(this);
+    }
+
+    handleChange(e) {
+        if (e.target.value.length <= 5) {
+            this.setState({guesses: this.state.guesses, textboxEntry: e.target.value, link: '', submitted:false});
+        } else {
+            this.setState({guesses: this.state.guesses, textboxEntry: this.state.textboxEntry,
+                           link: '', submitted:false});
+        }
+    }
+
+    handleSubmit() {
+        if (this.state.guesses.length < 6 && this.state.textboxEntry.length === 5) {
+            let newGuesses = this.state.guesses;
+            newGuesses.push(this.state.textboxEntry.toUpperCase());
+            this.setState({guesses: newGuesses, textboxEntry: '',
+            link: '', submitted:false});
+        }
+    }
+
+    handleSubmitAll() {
+        if (this.state.guesses.length < 2) {
+            alert("Please enter at least 2 guesses");
+        } else {
+            console.log(this.state.guesses);
+            let guessString = '';
+            for (let i = 0; i < this.state.guesses.length-1; i++) {
+                guessString += this.state.guesses[i]+',';
+            }
+            guessString += this.state.guesses[this.state.guesses.length-1];
+            let base64Val = Buffer.from(guessString).toString('base64');
+            console.log(base64Val);
+            let decodedVal = Buffer.from(base64Val, 'base64').toString('ascii');
+            console.log(decodedVal);
+            let linkStr = '/' + base64Val;
+            this.setState({guesses: this.state.guesses, textboxEntry: this.state.textboxEntry, submitted: true, link: linkStr});
+        }
+    }
+
+    render() {
+        if (this.state.submitted === true) {
+            return (<div id='WordleInputComponent'>
+            <p>Here's your link: {this.state.link} </p>
+            </div>);
+        }
+        let pastGuessTable = [];
+        for (let i = 0; i < this.state.guesses.length; i++) {
+            let tableEntry = <tr> <td> Guess # {i+1}: {this.state.guesses[i]} </td> </tr>;
+            pastGuessTable.push(tableEntry);
+        }
+        if (this.state.guesses.length >= 2) {
+            return (<div id='WordleInputComponent'>
+                <p> Enter your guesses on today's Wordle </p>
+                <table id='pastGuessTable'> { pastGuessTable } </table>
+                <p id='inputPrompt'> Enter guess # {this.state.guesses.length+1}: </p>
+                <input id='inputBox' type='text' 
+                value={ this.state.textboxEntry } onChange={ this.handleChange } />
+                <button id='inputSubmitButton' onClick={ this.handleSubmit }>
+                    Submit word
+                </button>
+                <button id='inputSubmitAllButton' onClick={ this.handleSubmitAll }>
+                    Submit Wordle    
+                </button>
+            </div>);
+        }
+        return (<div id='WordleInputComponent'>
+            <p> Enter your guesses on today's Wordle </p>
+                <table id='pastGuessTable'> { pastGuessTable } </table>
+                <p> Enter guess # {this.state.guesses.length+1}: </p>
+                <input id='inputBox' type='text' 
+                value={ this.state.textboxEntry } onChange={ this.handleChange } />
+                <button id='inputSubmitButton' onClick={ this.handleSubmit }>
+                    Submit word
+                </button>
+        </div>);
+    }
+}
+
+export default WordleInput;
